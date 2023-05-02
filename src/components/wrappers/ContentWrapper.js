1ad39bbce6fb8fa0c70f6224ae0b7898d6293a19
Alesia Korzun <liskorzun@gmail.com>
2023-05-02 19:13:03 +0300

    update animation

--- src/components/wrappers/ContentWrapper.js
+++ src/components/wrappers/ContentWrapper.js
@@ -3,13 +3,13 @@ import { motion } from 'framer-motion'
 export const ContentWrapper = ({ children }) => (
     <motion.main
         className="w-full mx-auto flex flex-col-reverse md:flex-row md:container"
-        initial={{ opacity: 0, x: 300 }}
+        initial={{ opacity: 0, x: 400 }}
         animate={{ opacity: 1, x: 0 }}
-        exit={{ opacity: 0, x: -300 }}
+        exit={{ opacity: 0, x: -400 }}
         transition={{
             type: 'spring',
-            stiffness: 250,
-            damping: 25,
+            stiffness: 280,
+            damping: 20,
         }}
     >
         {children}
